48e43bae746fcda9297f7b25a87d6335633fd123
0x5b62656e5d <ben@benkou.dev>
2026-04-25 12:05:43 +0800

    add encryption helper methods

---
+++ src/lib/encryption.ts
@@ -0,0 +1,32 @@
+import crypto from "node:crypto";
+import { env } from "@/env/server";
+
+export const encrypt = (input: string): string => {
+    const iv = crypto.randomBytes(env.IV_LENGTH);
+
+    const cipher = crypto.createCipheriv(
+        env.ENCRYPTION_METHOD,
+        crypto.scryptSync(env.ENCRYPTION_KEY, env.KDF_SALT, 32),
+        iv,
+    ) as crypto.CipherGCM;
+
+    let encrypted = cipher.update(input, "utf8", "hex");
+    encrypted += cipher.final("hex");
+
+    return `${iv.toString("hex")}:${cipher.getAuthTag().toString("hex")}:${encrypted}`;
+};
+
+export const decrypt = (input: string): string => {
+    const [ivHex, tagHex, encryptedText] = input.split(":");
+
+    const decipher = crypto.createDecipheriv(
+        env.ENCRYPTION_METHOD,
+        crypto.scryptSync(env.ENCRYPTION_KEY, env.KDF_SALT, 32),
+        Buffer.from(ivHex, "hex"),
+    ) as crypto.DecipherGCM;
+    decipher.setAuthTag(Buffer.from(tagHex, "hex"));
+
+    return (
+        decipher.update(encryptedText, "hex", "utf8") + decipher.final("utf8")
+    );
+};
