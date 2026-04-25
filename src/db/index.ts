96101c193cbbb64a3962dfc4c15c6161b8c76a4b
0x5b62656e5d <ben@benkou.dev>
2026-04-25 23:26:57 +0800

    fix db object schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,8 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { env } from "@/env/server";
+import * as authSchema from "./auth-schema.ts";
 import * as schema from "./schema.ts";
 
-export const db = drizzle(env.DATABASE_URL, { schema });
+export const db = drizzle(env.DATABASE_URL, {
+    schema: { ...schema, ...authSchema },
+});
