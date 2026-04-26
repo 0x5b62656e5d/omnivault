bd67c02b8e1c1fdded3422c4a8a029e5f3f5193b
0x5b62656e5d <ben@benkou.dev>
2026-04-26 13:12:21 +0800

    add health route

---
+++ src/routes/api/health/index.tsx
@@ -0,0 +1,21 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { createStandardResponse } from "@/lib/utils";
+
+export const Route = createFileRoute("/api/health/")({
+    server: {
+        handlers: {
+            GET: () => {
+                return new Response(
+                    JSON.stringify(
+                        createStandardResponse(
+                            true,
+                            `OK - ${new Date().toISOString()}`,
+                            "Server is healthy",
+                            null,
+                        ),
+                    ),
+                );
+            },
+        },
+    },
+});
