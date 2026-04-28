a71c098136a77c12177260431dc61d218b57ff1a
0x5b62656e5d <ben@benkou.dev>
2026-04-28 15:32:24 +0800

    add API endpoint to refetch buckets

---
+++ src/routes/api/s3/buckets/refetch/index.ts
@@ -0,0 +1,110 @@
+import type { ListBucketsCommandOutput } from "@aws-sdk/client-s3";
+import { createFileRoute } from "@tanstack/react-router";
+import { eq } from "drizzle-orm";
+import { db } from "@/db";
+import { s3credentials } from "@/db/schema";
+import { getSession } from "@/lib/auth.functions";
+import { decrypt } from "@/lib/encryption";
+import { loadBucketRegions, loadBuckets } from "@/lib/s3/buckets";
+import { createStandardResponse } from "@/lib/utils";
+
+export const Route = createFileRoute("/api/s3/buckets/refetch/")({
+    server: {
+        handlers: {
+            PUT: async () => {
+                const session = await getSession();
+
+                if (!session) {
+                    return new Response(
+                        JSON.stringify(
+                            createStandardResponse(
+                                false,
+                                null,
+                                "Unauthorized",
+                                null,
+                            ),
+                        ),
+                        {
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            status: 401,
+                        },
+                    );
+                }
+
+                const res = await db
+                    .select()
+                    .from(s3credentials)
+                    .where(eq(s3credentials.ownedBy, session.user.id));
+
+                for (const credential of res) {
+                    let buckets: ListBucketsCommandOutput;
+
+                    if (credential.endpointUrl) {
+                        buckets = await loadBuckets("auto", {
+                            accessKeyId: decrypt(credential.accessKeyId),
+                            secretAccessKey: decrypt(
+                                credential.secretAccessKey,
+                            ),
+                            endpointUrl: credential.endpointUrl,
+                        });
+                    } else {
+                        buckets = await loadBuckets("auto", {
+                            accessKeyId: decrypt(credential.accessKeyId),
+                            secretAccessKey: decrypt(
+                                credential.secretAccessKey,
+                            ),
+                        });
+                    }
+
+                    const bucketNames =
+                        buckets.Buckets?.map(bucket => bucket.Name || "") || [];
+
+                    if (bucketNames) {
+                        if (credential.endpointUrl) {
+                            await loadBucketRegions(
+                                session.user.id,
+                                {
+                                    accessKeyId: decrypt(
+                                        credential.accessKeyId,
+                                    ),
+                                    secretAccessKey: decrypt(
+                                        credential.secretAccessKey,
+                                    ),
+                                    endpointUrl: credential.endpointUrl,
+                                },
+                                ...bucketNames,
+                            );
+                        } else {
+                            await loadBucketRegions(
+                                session.user.id,
+                                {
+                                    accessKeyId: decrypt(
+                                        credential.accessKeyId,
+                                    ),
+                                    secretAccessKey: decrypt(
+                                        credential.secretAccessKey,
+                                    ),
+                                },
+                                ...bucketNames,
+                            );
+                        }
+                    }
+                }
+
+                return new Response(
+                    JSON.stringify(
+                        createStandardResponse(false, null, null, null),
+                    ),
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        status: 200,
+                    },
+                );
+            },
+        },
+    },
+});
