318054dd560d59358263238727e59456a0107d44
0x5b62656e5d <ben@benkou.dev>
2026-04-29 15:18:30 +0800

    refactor createClient

--- src/lib/s3/client.ts
+++ src/lib/s3/client.ts
@@ -2,24 +2,17 @@ import { S3Client } from "@aws-sdk/client-s3";
 import type { S3Credential } from "../types";
 
 export const createClient = (region: string, s3Credential: S3Credential) => {
-    return s3Credential.endpointUrl
-        ? new S3Client({
-              region: region,
-              endpoint: s3Credential.endpointUrl,
-              credentials: {
-                  accessKeyId: s3Credential.accessKeyId,
-                  secretAccessKey: s3Credential.secretAccessKey,
-              },
-              forcePathStyle: true,
-          })
-        : new S3Client({
-              region: region,
-              credentials: {
-                  accessKeyId: s3Credential.accessKeyId,
-                  secretAccessKey: s3Credential.secretAccessKey,
-              },
-              forcePathStyle: true,
-          });
+    return new S3Client({
+        region: region,
+        endpoint: s3Credential.endpointUrl || undefined,
+        credentials: {
+            accessKeyId: s3Credential.accessKeyId,
+            secretAccessKey: s3Credential.secretAccessKey,
+        },
+        forcePathStyle: true,
+    });
+};
+
 export const getRegion = (endpointUrl: string): string | null => {
     try {
         const hostname = new URL(endpointUrl).hostname;
