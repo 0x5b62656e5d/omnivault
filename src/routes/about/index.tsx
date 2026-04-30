372d39faf51af46620a87df7717cfadb7eb13d35
0x5b62656e5d <ben@benkou.dev>
2026-04-30 20:45:26 +0800

    add about page

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -1,9 +1,25 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { FiArrowUpRight } from "react-icons/fi";
 
 export const Route = createFileRoute("/about/")({
     component: RouteComponent,
 });
 
 function RouteComponent() {
-    return <div>About page</div>;
+    return (
+        <div className="flex w-screen min-h-full p-4">
+            <div className="flex flex-col w-full items-center gap-4">
+                <h1 className="text-4xl font-medium">About Omnivault</h1>
+                <a
+                    href="https://github.com/0x5b62656e5d/omnivault"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-flex justify-center items-center transition-transform duration-250 ease-in-out hover:scale-[1.025]"
+                >
+                    <u>GitHub Repository</u>
+                    <FiArrowUpRight />
+                </a>
+            </div>
+        </div>
+    );
 }
