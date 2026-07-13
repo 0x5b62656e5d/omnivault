fc7ee01f51a854c0655e834d301e57f179a0dc4b
0x5b62656e5d <ben@benkou.dev>
2026-07-14 07:37:41 +0800

    update notFound component styling

--- src/components/notFound.tsx
+++ src/components/notFound.tsx
@@ -1,10 +1,14 @@
 export const NotFound = (): React.ReactNode => {
     return (
-        <div className="w-full h-full flex flex-col justify-center items-center">
+        <div className="w-full h-full flex flex-col justify-center items-center gap-2">
             <h1>
                 The page you have requested does not exist or has been moved.
             </h1>
-            <button onClick={() => window.history.back()} type="button">
+            <button
+                className="border-2 px-2 py-1 rounded hover:cursor-pointer"
+                onClick={() => window.history.back()}
+                type="button"
+            >
                 Go Back
             </button>
         </div>
